ee7e07d118fd71f426b0f9f8908d11bb4fec2e8e
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-01-02 01:03:05 +0900

    refactor: テストコードを直す
    プロダクトコードは優先でリファクタリングだが、特に直すところなし。
    テストコードをインライン化する。

--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -7,10 +7,8 @@ class TestFizzBuzz(object):
     def test_1を渡すと文字列1を返す(self):
         # 準備
         fizz_buzz = FizzBuzz()
-        # 実行
-        actual = fizz_buzz.convert(1)
-        # 検証
-        assert actual == '1' 
+        # 実行 & 検証
+        assert fizz_buzz.convert(1) == '1'
 
 
 if __name__ == '__main__':
