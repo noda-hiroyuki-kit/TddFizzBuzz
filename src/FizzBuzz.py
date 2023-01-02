8a64c2bbb63f176eaab60c846181c5e926c55b88
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-01-02 16:16:22 +0900

    feat: 「15を渡すと文字列FizzBuzzを返す」を実装
    明白な実装でプロダクトコードに追加

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,5 +1,7 @@
 class FizzBuzz:
     def convert(self, num: int):
+        if num % 15 == 0:
+            return 'FizzBuzz'
         if num % 3 == 0:
             return 'Fizz'
         if num % 5 == 0:
