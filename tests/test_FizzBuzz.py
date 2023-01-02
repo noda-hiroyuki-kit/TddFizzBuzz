e11a40f7bff7582785e365d8dc8d6bc4d6ca5813
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-01-02 16:44:46 +0900

    refactor: testコードを階層化する
    インナークラスにして、テストコードを階層化しテストコードを仕様書化する。

--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -11,20 +11,25 @@ def 前準備():
 
 
 class TestFizzBuzz(object):
-    def test_1を渡すと文字列1を返す(self, 前準備):
-        assert fizz_buzz.convert(1) == '1'
+    class Test_数を文字列に変換する(object):
+        def test_1を渡すと文字列1を返す(self, 前準備):
+            assert fizz_buzz.convert(1) == '1'
 
-    def test_2を渡すと文字列2を返す(self, 前準備):
-        assert fizz_buzz.convert(2) == '2'
+        def test_2を渡すと文字列2を返す(self, 前準備):
+            assert fizz_buzz.convert(2) == '2'
 
-    def test_3を渡すと文字列Fizzを返す(self, 前準備):
-        assert fizz_buzz.convert(3) == 'Fizz'
+    class Test_3の倍数のときは数の代わりにFizzに変換する(object):
+        def test_3を渡すと文字列Fizzを返す(self, 前準備):
+            assert fizz_buzz.convert(3) == 'Fizz'
 
-    def test_5を渡すと文字列Buzzを返す(self, 前準備):
-        assert fizz_buzz.convert(5) == 'Buzz'
-            
-    def test_15を渡すと文字列FizzBuzzを返す(self, 前準備):
-        assert fizz_buzz.convert(15) == 'FizzBuzz'
+    class Test_5の倍数のときは数の代わりにBuzzに変換する(object):
+        def test_5を渡すと文字列Buzzを返す(self, 前準備):
+            assert fizz_buzz.convert(5) == 'Buzz'
+
+    class Test_3と5両方の倍数のときは数の代わりにFizzBuzzに変換する(object):
+        def test_15を渡すと文字列FizzBuzzを返す(self, 前準備):
+            assert fizz_buzz.convert(15) == 'FizzBuzz'
+
 
 if __name__ == '__main__':
     pytest.main()
