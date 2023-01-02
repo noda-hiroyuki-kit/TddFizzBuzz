611b3366760c76e5c7ce6ac691f1f133ab1d647c
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-01-02 15:06:09 +0900

    refactor: 前準備をクラスの外に出す
    3回重複していた前準備をクラスの外に出し、重複をなくす。

--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -3,23 +3,21 @@ import pytest
 from ..src.FizzBuzz import FizzBuzz
 
 
+@pytest.fixture()
+def 前準備():
+    global fizz_buzz
+    fizz_buzz = FizzBuzz()
+    yield fizz_buzz
+
+
 class TestFizzBuzz(object):
-    def test_1を渡すと文字列1を返す(self):
-        # 準備
-        fizz_buzz = FizzBuzz()
-        # 実行 & 検証
+    def test_1を渡すと文字列1を返す(self, 前準備):
         assert fizz_buzz.convert(1) == '1'
 
-    def test_2を渡すと文字列2を返す(self):
-        # 準備
-        fizz_buzz = FizzBuzz()
-        # 実行 & 検証
+    def test_2を渡すと文字列2を返す(self, 前準備):
         assert fizz_buzz.convert(2) == '2'
 
-    def test_3を渡すと文字列Fizzを返す(self):
-        # 準備
-        fizz_buzz = FizzBuzz()
-        # 実行 & 検証
+    def test_3を渡すと文字列Fizzを返す(self, 前準備):
         assert fizz_buzz.convert(3) == 'Fizz'
 
 
