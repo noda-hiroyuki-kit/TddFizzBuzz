6f2b8f663e540fb5246fd70fe2dbdc7830a1372b
noda-hiroyuki-kit <noda.hiroyuki@yk.commufa.jp>
2023-01-02 17:02:23 +0900

    refactor: 他のテストと粒度を揃える
    三角測量のために追加した「2を渡すと文字列2を返す」のテストを消す

--- tests/test_FizzBuzz.py
+++ tests/test_FizzBuzz.py
@@ -29,10 +29,6 @@ class TestFizzBuzz(object):
             def test_1を渡すと文字列1を返す(self, 前準備):
                 assert fizz_buzz.convert(1) == '1'
 
-            def test_2を渡すと文字列2を返す(self, 前準備):
-                assert fizz_buzz.convert(2) == '2'
-
-
 
 if __name__ == '__main__':
     pytest.main()
